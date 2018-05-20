e4ab250511683989aef0afa5fe67235c21eb6588
Nikunj Parmar <nikunjparmar828@gmail.com>
2018-05-20 12:25:49 +0530

    Update web_spider.py

--- web_spider.py
+++ web_spider.py
@@ -6,7 +6,7 @@ def web_spider(max_pages):
 	while page <= max_page:
 		url = 'https://buckysroom.org/trade/search.php?page=' + str(page)
 		source = requests.get(url)
-		plain_text = source.tex
+		plain_text = source.text
 		soup = BeautifulSoup(plain_text)
 		for link in soup.findAll('a', {'class': 'item-name'}):
 			href = "https://buckysroom.org" + link.get('href')
